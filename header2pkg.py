10bcdba2821c4f99faced842b1ae2b4460710094
jadie <yugioh072@163.com>
2012-06-19 23:52:00 +0800

    -[TOLUA]move file to sub folder

--- header2pkg.py
+++
@@ -1,6 +0,0 @@
-def ConverHeader2PKG(filePath):
-    fileContent = file(filePath)
-    fileLines = fileContent.readlines()
-    for curLine in fileLines:
-        curLine = curLine.strip()
-        if
